74471e2af2eb2f8b1718c192e0c7e490a57ae804
Tristan Waldear <twaldear@zynga.com>
2015-04-22 17:08:49 -0700

    fix setup.py/manifest.in

---
+++ flask_csp/csp.py
@@ -0,0 +1,64 @@
+from flask import make_response
+from functools import wraps
+import os
+import json
+
+class csp_default:
+	default_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),'csp_default.json')
+	
+	def read(self):
+		""" read default csp settings from json file """
+		with open(self.default_file) as json_file:
+			try:
+				return json.load(json_file)
+			except Exception, e:
+				raise 'empty file'
+			
+	def update(self,updates={}):
+		""" 
+			update csp_default.json with dict
+			
+			if file empty add default-src and create dict
+		"""
+		try:
+			csp = self.read()
+		except:
+			csp = {'default-src':"'self'"}
+			self.write(csp)
+		csp.update(updates)
+		self.write(csp)
+		
+	def write(self,content):
+		fh = open(self.default_file, "w")
+		fh.write(json.dumps(content))
+		fh.close()
+
+def create_csp_header(cspDict):
+	""" create csp header string """
+	policy = ['%s %s' % (k, v) for k, v in cspDict.items() if v != '']
+	return '; '.join(policy)
+
+def csp_header(csp={}):
+    """ Decorator to include csp header on app.route wrapper """
+    _csp = csp_default().read()
+    _csp.update(csp)
+    
+    _header = ''
+    if 'report-only' in _csp and _csp['report-only'] is True:
+    	_header = 'Content-Security-Policy-Report-Only'
+    else:
+    	_header = 'Content-Security-Policy'
+    if 'report-only' in _csp:
+    	del _csp['report-only']
+    _headers = {_header: create_csp_header(_csp)}
+    
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+            h = resp.headers
+            for header, value in _headers.items():
+                h[header] = value
+            return resp
+        return decorated_function
+    return decorator
--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import setup
 
+import flask_csp
+
 setup(
   name = 'flask-csp',
-  packages = ['flask-csp'],
-  version = '0.1',
+  py_modules = ['flask_csp'],
+  packages = ['flask_csp'],
+  include_package_data = True,
+  package_data = {'':['csp_default.json']},
+  version = '0.37',
   description = 'Flask Content Security Policy header support',
   license='MIT',
   author = 'Tristan Waldear',
   author_email = 'trwaldear@gmail.com',
   url = 'https://github.com/twaldear/flask-csp',
   download_url = 'https://github.com/twaldear/flask-csp/tarball/0.1',
-  install_requires=[
-        'Flask'
-  ],
-  classifiers = [],
+  keywords = ['flask', 'csp', 'header'],
+  classifiers=[
+    'Programming Language :: Python :: 2',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ]
 )
